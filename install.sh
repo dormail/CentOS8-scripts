e8648d1d2fc477381fdaf379ef7a779831cbd1d0
Matthias Maile <matthias.maile@udo.edu>
2020-10-18 04:45:33 +0200

    stupid comments added

--- install.sh
+++ install.sh
@@ -27,5 +27,8 @@ yum -y update
 yum -y module reset php
 yum -y module enable php:remi-7.2
 
-yum search php-pecl-redis
 yum install -y php-pecl-redis
+
+# we will use nginx 
+# since we have an extra subdomain we put it in the webroot of this nginx server
+# and we will copy the file since I am to lazy to work with bash scripting
