44f3020dd200365b19c2b111774a3338189c3925
dormail <59531240+dormail@users.noreply.github.com>
2020-10-18 04:24:16 +0200

    Add files via upload
    For Some reason all my single commits failed during push
    
    Only installing the php stuff implemented

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# as a database we use MySQL/MariadDB
+
+# get all the necessary packages
+# those are the ones that are either necessary or boost the performance
+# if you need to do some fancy stuff maybe some other php packages are needed, read
+# https://docs.nextcloud.com/server/20/admin_manual/installation/source_installation.html#prerequisites-for-manual-installation
+yum update
+yum install -y php \
+	php-dom \
+	php-gd \
+	php-json \
+	php-mbstring \
+	php-posix \
+	php-zip \
+	php-pdo_mysql \
+	php-intl \
+	redis
+
+# for the caching module redis we need to instsall remi repo which we get through the epel repo
+# php 7.2 was the version cent os gave me so I went with it - ADJUST TO YOUR NEEDS
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+yum -y update
+yum -y install https://rpms.remirepo.net/enterprise/remi-release-8.rpm
+yum -y update
+
+yum -y module reset enable php:remi-7.2
+yum -y module enable php:remi-7.2
+
+yum search php-pecl-redis
+yum install -y php-pecl-redis
