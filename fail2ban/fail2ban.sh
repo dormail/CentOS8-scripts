25978628fcf5e4f70564f268039daa85bc23df11
Matthias Maile <matthias.maile@udo.edu>
2020-10-25 16:59:37 +0100

    copying file added to script

--- fail2ban/fail2ban.sh
+++ fail2ban/fail2ban.sh
@@ -5,6 +5,8 @@ yum -y update
 yum -y install epel-release
 yum -y install fail2ban
 
+# copying config file
+cp ./jail.local /etc/fail2ban/jail.local
 
 # Activating fail2ban
 systemctl start fail2ban
